66d740c5a4f10d1e9328a4588afc5c085d7c7b4c
jsoumbundit <so_jirawat@yahoo.com>
2018-08-11 23:27:14 +0700

    Initial commit

---
+++ app/src/main/java/thailand/soumbundit/jirawat/funnyquestion/MainActivity.java
@@ -0,0 +1,26 @@
+package thailand.soumbundit.jirawat.funnyquestion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import thailand.soumbundit.jirawat.funnyquestion.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Add Fragment to Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment,new MainFragment())
+                    .commit();
+
+        }
+
+
+    }
+}
